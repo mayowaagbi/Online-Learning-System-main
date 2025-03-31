c17c04abb575012420e3a5947c07836b1645e504
mayowaagbi <agbimayowa3@gmail.com>
2025-03-31 11:04:45 +0100

    probably final commit

--- dash.php
+++ dash.php
@@ -132,7 +132,8 @@ echo '</table></div></div>';
 //ranking start
 if(@$_GET['q']== 2) 
 {
-$q=mysqli_query($con,"SELECT * FROM rank  ORDER BY score DESC " )or die('Error223');
+$q = mysqli_query($con, "SELECT * FROM `rank` ORDER BY score DESC") or die(mysqli_error($con));
+
 echo  '<div class="panel title"><div class="table-responsive">
 <table class="table table-striped title1" >
 <tr style="color:red"><td><b>Rank</b></td><td><b>Name</b></td><td><b>Gender</b></td><td><b>College</b></td><td><b>Score</b></td></tr>';
